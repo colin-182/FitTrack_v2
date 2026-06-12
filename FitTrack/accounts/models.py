1a32c393d76f99d7b31cf2d9bc0162d1053fa455
Colin Keogh <colin-182@hotmail.com>
2026-06-12 20:10:27 +0100

    updated models.py
    models.py updated with customeruser model using member and trainer channels

--- FitTrack/accounts/models.py
+++ FitTrack/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('member', 'Member'),
+        ('trainer', 'Trainer'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='member')
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True)
+    phone = models.CharField(max_length=20, blank=True)
