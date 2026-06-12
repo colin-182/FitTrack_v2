650e1bb38ada4b9154c9d2541c5fd98790e0135e
Colin Keogh <colin-182@hotmail.com>
2026-06-12 20:44:23 +0100

    admin.py updated
    register models in workouts>admin updated to view in admin panel

--- FitTrack/workouts/admin.py
+++ FitTrack/workouts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Category, WorkoutPlan, WorkoutLog
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(WorkoutPlan)
+admin.site.register(WorkoutLog)
